13cea5209ec4c2bd34cc82c49b8d5428414942f2
mpetavy <mpetavy>
2023-07-30 21:13:52 +0200

    debian build, separate build outputs

--- build-alpine.sh
+++ build-alpine.sh
@@ -3,6 +3,12 @@
 set -e # stop on error in script
 set -x # log script steps to STDOUT
 
+export target=./bin-alpine
+
+[ -d "$target" ] && rm -rf $target
+
+mkdir $target
+
 export APP=osslsigncode
 export DOCKER_IMAGE=alpine:latest
 export OSSLSIGNCODE_VERSION=2.6
@@ -11,7 +17,7 @@ docker image build --force-rm --rm  -t $APP --build-arg DOCKER_IMAGE=$DOCKER_IMA
 
 docker container create --name $APP-temp $APP
 
-docker cp $APP-temp:/osslsigncode ./osslsigncode-alpine
+docker cp $APP-temp:/osslsigncode $target/
 
 docker container rm $APP-temp
 docker image prune -f
---
+++ build-debian.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+set -e # stop on error in script
+set -x # log script steps to STDOUT
+
+target=./bin-debian
+
+[ -d "$target" ] && rm -rf $target
+
+mkdir $target
+
+APP=osslsigncode
+DOCKER_IMAGE=debian:bookworm-slim
+OSSLSIGNCODE_VERSION=2.6
+
+docker image build --force-rm --rm  -t $APP --build-arg DOCKER_IMAGE=$DOCKER_IMAGE --build-arg OSSLSIGNCODE_VERSION=$OSSLSIGNCODE_VERSION -f ./Dockerfile-debian .
+
+docker container create --name $APP-temp $APP
+
+docker cp $APP-temp:/osslsigncode $target
+
+docker container rm $APP-temp
+docker image prune -f
